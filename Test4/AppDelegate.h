afc386a62275d58fe9048df8c1c6a9237243fb38
intern <intern@admins-iMac.local>
2017-07-03 14:57:02 +0300

    Initial Commit

---
+++ Test4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test4
+//
+//  Created by intern on 03/07/2017.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test4
+//
+//  Created by intern on 03/07/2017.
+//  Copyright © 2017 intern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
